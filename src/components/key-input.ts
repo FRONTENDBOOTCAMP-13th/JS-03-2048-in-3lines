61bbae7798e510a57744819ed80f04e78232cc65
Dongsusin <auroratime020715@gmail.com>
2025-05-13 15:41:22 +0900

    ✨key-input

---
+++ src/components/key-input.ts
@@ -0,0 +1,16 @@
+// 키보드 입력 처리
+document.addEventListener("keydown", (event: KeyboardEvent) => {
+    switch (event.key) {
+        case "ArrowUp":
+            break;
+        case "ArrowDown":
+            break;
+        case "ArrowLeft":
+            break;
+        case "ArrowRight":
+            break;
+    }
+});
+
+// 이동 방향 타입
+type Direction = "up" | "down" | "left" | "right";
