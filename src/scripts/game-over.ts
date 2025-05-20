9c61d271e8fae1aedf09c02abe032c055122c9d9
Dongsusin <auroratime020715@gmail.com>
2025-05-20 23:51:57 +0900

    [feature] ai mode

--- src/scripts/game-over.ts
+++ src/scripts/game-over.ts
@@ -1,7 +1,8 @@
 import { boardSize } from "./boardsize";
-import { grid } from "./add-random-cell";
+import { grid, grid2 } from "./add-random-cell";
 import { resetScore } from "./score";
 import { backupGridState } from "./game-start";
+import { isAIMode } from "../main";
 
 export let isGameOver = false;
 export let isTimeAttackMode = false; // 타임어택 모드 여부를 저장
@@ -33,15 +34,52 @@ function canMoveOrMerge(): boolean {
 }
 
 // 이미지 띄우기
-function showGameOverImage(): void {
+export function showGameOverImage(): void {
+    const gameOver1pEl = document.getElementById("game-over-1p");
     const gameOverEl = document.getElementById("game-over");
     resetScore();
     backupGridState();
-    if (gameOverEl) {
-        gameOverEl.style.display = "flex";
+    if (isAIMode) {
+        if (gameOver1pEl) {
+            gameOver1pEl.style.display = "flex";
+        }
+    } else {
+        if (gameOverEl) {
+            gameOverEl.style.display = "flex";
+        }
+    }
+}
+export function showGameOver2pImage(): void {
+    const gameOver2pEl = document.getElementById("game-over-2p");
+    if (gameOver2pEl) {
+        gameOver2pEl.style.display = "flex";
     }
 }
 
+// 게임 오버 조건 확인 및 이미지 표시
+export function checkGameOver2(): void {
+    const canContinue = canMoveOrMerge2();
+    if (!canContinue) {
+        showGameOver2pImage();
+    }
+}
+
+// 이동 또는 병합 가능한 타일이 있는지 확인
+function canMoveOrMerge2(): boolean {
+    for (let row = 0; row < boardSize; row++) {
+        for (let col = 0; col < boardSize; col++) {
+            const current = grid2[row][col];
+            if (current === 0) return true;
+
+            // 오른쪽
+            if (col < boardSize - 1 && grid2[row][col + 1] === current) return true;
+
+            // 아래쪽
+            if (row < boardSize - 1 && grid2[row + 1][col] === current) return true;
+        }
+    }
+    return false;
+}
 export function timeAttack(): void {
     isTimeAttackMode = true;
     if (timeAttackTimeoutId !== null) {
